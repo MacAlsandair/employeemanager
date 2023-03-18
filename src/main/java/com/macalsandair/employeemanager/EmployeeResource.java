5e9f1ba8091fff9b2b297a320e4dba70c3030502
MacAlsandair <mac.alsandair@gmail.com>
2023-03-18 17:17:04 +0200

    Add put and delete methods in EmployeeResource

--- src/main/java/com/macalsandair/employeemanager/EmployeeResource.java
+++ src/main/java/com/macalsandair/employeemanager/EmployeeResource.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,12 +41,18 @@ public class EmployeeResource {
 	@PostMapping("/add")
 	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
 		Employee newEmployee = employeeService.addEmployee(employee);
-		return new ResponseEntity<>(employee, HttpStatus.CREATED);
+		return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
 	}
 	
-	@PostMapping("/add")
-	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
-		Employee newEmployee = employeeService.addEmployee(employee);
-		return new ResponseEntity<>(employee, HttpStatus.CREATED);
+	@PutMapping("/update")
+	public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+		Employee newEmployee = employeeService.updateEmployee(employee);
+		return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+		employeeService.deleteEmployee(id);
+		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 }
